cd05dd8ca9f28ea36e3b191a1d3eeb57b80e93d1
liangzuobin <liangzuobin@meican.com>
2018-12-12 16:37:08 +0800

    anything else?

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    guess_number()
-}
-
-fn guess_number() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
     // println!("The secret number is: {}", secret_number);
+
+    let parse_u32 = |s: String| -> Result<u32, std::num::ParseIntError> {
+        let i: u32 = s.trim().parse()?;
+        Ok(i)
+    };
 
     loop {
         println!("Please input your guess.");
@@ -19,28 +20,19 @@ fn guess_number() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: Result<u32, std::num::ParseIntError> = parse_u32(guess);
-
-        if !guess.is_ok() {
-            println!("Please type a number");
-            continue;
-        }
-
-        let guess: u32 = guess.unwrap();
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small"),
-            Ordering::Greater => println!("Too big"),
-            Ordering::Equal => {
-                println!("You win");
-                break;
+        match parse_u32(guess) {
+            Ok(guess) => match guess.cmp(&secret_number) {
+                Ordering::Less => println!("Too small"),
+                Ordering::Greater => println!("Too big"),
+                Ordering::Equal => {
+                    println!("You win");
+                    break;
+                }
+            },
+            Err(_) => {
+                println!("Please type a number");
+                continue;
             }
         }
     }
-}
-
-fn parse_u32(s: String) -> Result<u32, std::num::ParseIntError> {
-    let i: u32 = s.trim().parse()?;
-    Ok(i)
 }
